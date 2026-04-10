c3f677a53bcde1bee5a74682492af6b198fb9f0c
DomsVesuvio <domenicocorleto12@gmail.com>
2026-04-10 09:44:46 +0200

    Update deltaG_sep_acqua.py

--- deltaG_sep_acqua.py
+++ deltaG_sep_acqua.py
@@ -13,15 +13,15 @@ muZnSe=-6790.5153695988*27.2114
 
 
 muZnOCu_OH_ads=-151501.20362725505
-muZnOCu_H_ads=
+muZnOCu_H_ads=-151063.28805820455
 muZnOCu_H2O_ads=-151518.10185170427
 
 muZnSCu_OH_ads=-186894.26043731655
-muZnSCu_H_ads=
+muZnSCu_H_ads=-186456.03641370084
 muZnSCu_H2O_ads=-186910.6294842915
 
 muZnSeCu_OH_ads=-184878.72692001172
-muZnSeCu_H_ads=
+muZnSeCu_H_ads=-184439.97319227585
 muZnSeCu_H2O_ads=-184894.79660432605
 
 #DeltaG1= muZnO_OH_ads + muZnO_H_ads - muZnO_H2O_ads - muZnO
@@ -32,9 +32,9 @@ DeltaG_ZnO_doped= muZnOCu_OH_ads + muZnOCu_H_ads - muZnOCu_H2O_ads - muZnO_Cu
 DeltaG_ZnS_doped= muZnSCu_OH_ads + muZnSCu_H_ads - muZnSCu_H2O_ads - muZnS_Cu
 DeltaG_ZnSe_doped= muZnSeCu_OH_ads + muZnSeCu_H_ads - muZnSeCu_H2O_ads - muZnSe_Cu
 
-print(DeltaG4)
-print(DeltaG5)
-print(DeltaG6)
+print(DeltaG_ZnO_doped)
+print(DeltaG_ZnS_doped)
+print(DeltaG_ZnSe_doped)
 
 
 #DeltaG di ZnO_Cu è 2.279301167349331
